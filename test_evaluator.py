66efa78de5f582e792b753fd67234be165404e28
Spencer Michalski <smichals@terpmail.umd.edu>
2021-06-02 17:11:53 -0400

    Test case for BUY decision

--- test_evaluator.py
+++ test_evaluator.py
@@ -1,12 +1,12 @@
-import helper_functions
+from helper_functions import *
 import unittest
 
 class TestEvaluator(unittest.TestCase):
 
 	def test_eps_conversion(self):
-		self.assertEqual(8.28, helper_functions.convert_eps_str_to_float("8.28"))
-		self.assertEqual(0.0, helper_functions.convert_eps_str_to_float("0"))
-		self.assertEqual(-3.1, helper_functions.convert_eps_str_to_float("(3.1)"))
+		self.assertEqual(8.28, convert_eps_str_to_float("8.28"))
+		self.assertEqual(0.0, convert_eps_str_to_float("0"))
+		self.assertEqual(-3.1, convert_eps_str_to_float("(3.1)"))
 
 	def test_conservative_financing(self):
 		total_assets = 52150.0
@@ -14,7 +14,9 @@ class TestEvaluator(unittest.TestCase):
 		total_shareholder_equity = 22230.0
 		longterm_debt = 10890.0
 
-		self.assertTrue(helper_functions.is_conservatively_financed(total_assets, total_liabilities, total_shareholder_equity, longterm_debt))
+		balancesheet = BalanceSheet(total_assets, total_liabilities, total_shareholder_equity, longterm_debt)
+
+		self.assertTrue(balancesheet.is_conservatively_financed())
 
 	def test_non_conservative_financing(self):
 		total_assets = 323890.0
@@ -22,4 +24,31 @@ class TestEvaluator(unittest.TestCase):
 		total_shareholder_equity = 65340.0
 		longterm_debt = 107050.0
 
-		self.assertFalse(helper_functions.is_conservatively_financed(total_assets, total_liabilities, total_shareholder_equity, longterm_debt))
+		balancesheet = BalanceSheet(total_assets, total_liabilities, total_shareholder_equity, longterm_debt)
+
+		self.assertFalse(balancesheet.is_conservatively_financed())
+
+	def test_decide_to_buy_company(self):
+		marketcap = 229720
+		prominent = True
+		total_assets = 153090
+		total_liabilities = 72050
+		total_shareholder_equity = 81040
+		longterm_debt = 34250
+		consistent_dividends = True
+		diluted_eps_history = [3.65, 3.65, 3.65, 3.65, 3.65, 3.65, 3.65, 3.65, 3.65, 3.65]
+		last_yr_basic_eps = 4.98
+		current_market_price = 57.42
+
+		ticker_data = TickerData(
+			'ABCDE', 
+			marketcap,
+			prominent,
+			BalanceSheet(total_assets, total_liabilities, total_shareholder_equity, longterm_debt),
+			consistent_dividends,
+			IncomeStatement(diluted_eps_history, last_yr_basic_eps),
+			current_market_price)
+
+		self.assertTrue(ticker_data.is_sound())
+		self.assertTrue(ticker_data.is_reasonably_priced())
+		self.assertTrue(ticker_data.should_buy())
